413b4ddab6f2a785285c207f936e69ae02aa64f5
RomainPct <romain.penchenat@gmail.com>
2021-07-09 16:19:55 +0200

    🤝 Initialize client server relation

--- server.js
+++ server.js
@@ -1,33 +1,59 @@
-let http = require('http');
-let server = http.createServer()
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
 
+/*
+* Classic server
+*/
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static('public'))
+app.get('/', function (req, res) {
+   res.sendFile('index.html', { root: __dirname })
+})
+
+app.get('/json', function (req, res) {
+   res.status(200).json({"message":"ok"})
+})
+
+/*
+* Socket IO
+*/
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+io.on('connection', (socket) =>{
+    console.log(`Connecté au client ${socket.id}`)
+})
+
+
+/*
+* Port reading
+*/
 var serialport = require('serialport');
 var portName = process.argv[2];
 
-var myPort = new serialport(portName, {
-    baudRate: 9600,
-    parser: new serialport.parsers.Readline('\r\n')
-});
+// var myPort = new serialport(portName, {
+//     baudRate: 9600,
+//     parser: new serialport.parsers.Readline('\r\n')
+// })
 
-server.on('request', function(request, response) {
+// setInterval(_ => {
+//     onData({'now': Date.now()})
+// }, 1000)
 
-    response.writeHead(200, {
-        'Content-type': 'text/html; charset=utf-8'
-    })
+myPort.on('open', onOpen);
+myPort.on('data', onData);
 
-    myPort.on('open', onOpen);
-    myPort.on('data', onData);
+function onOpen(){
+    console.log('Open connections!');
+}
 
-    function onOpen(){
-        console.log('Open connections!');
-    }
+function onData(data){
+    io.emit('newDataFromInput', data)
+    //console.log('data : ' + data);
+}
 
-    function onData(data){
-        response.end('data : ' + data);
-        //console.log('data : ' + data);
-    }
-});
-
-server.listen(8080);
-
-
+// App launching
+server.listen(3000, function () {
+    console.log('Votre app est disponible sur localhost:3000 !')
+})
